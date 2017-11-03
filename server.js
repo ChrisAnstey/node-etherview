c3e3ae1512d69bc8b2f6e91f3e88bf8cc22ac154
Chris Anstey <git@cpanstey.co.uk>
2017-11-03 00:19:11 +0000

    Code format tidy-up

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 /**
  * Basic web app to view the ethereum blockchain
  */
-
 require('dotenv').config()
 
 const http = require('http')
-const url  = require('url');
+const url = require('url');
 const Web3 = require('web3');
 const ejs = require('ejs')
 
@@ -16,43 +15,45 @@ const ejs = require('ejs')
  * @return {[type]}          [description]
  */
 const requestHandler = (request, response) => {
-  response.writeHead(200, {'content-type': 'text/html'});
+    response.writeHead(200, {
+        'content-type': 'text/html'
+    });
 
-  // pass true as 2nd param to parse querystring too
-  var reqDeets = url.parse(request.url, true)
+    // pass true as 2nd param to parse querystring too
+    var reqDeets = url.parse(request.url, true)
 
-  // create connection to geth node - in this case, the docker host
-  var web3 = new Web3(new Web3.providers.HttpProvider(process.env.WEB3_PROVIDER))
+    // create connection to geth node - in this case, the docker host
+    var web3 = new Web3(new Web3.providers.HttpProvider(process.env.WEB3_PROVIDER))
 
-  // load the block info using web3
-  var block = web3.eth.getBlock(reqDeets.query.block)
+    // load the block info using web3
+    var block = web3.eth.getBlock(reqDeets.query.block)
 
-  ejs.renderFile(__dirname + '/views/pages/block.ejs',
-  	{
-  		title : 'View Block: ' + block.number,
-  		hash  : block.hash, block: block
-  	},
-  	function(err, result) {
-		// render on success
-		if (!err) {
-			response.end(result);
-		}
-		// render or error
-		else {
-			response.end('An error occurred');
-			console.log(err);
-		}
-	}
-  );
+    ejs.renderFile(__dirname + '/views/pages/block.ejs', {
+            title: 'View Block: ' + block.number,
+            hash: block.hash,
+            block: block
+        },
+        function(err, result) {
+            // render on success
+            if (!err) {
+                response.end(result);
+            }
+            // render or error
+            else {
+                response.end('An error occurred');
+                console.log(err);
+            }
+        }
+    );
 
 }
 
 const server = http.createServer(requestHandler)
 
 server.listen(process.env.HTTP_PORT, (err) => {
-  if (err) {
-    return console.log('something bad happened', err)
-  }
+    if (err) {
+        return console.log('something bad happened', err)
+    }
 
-  console.log(`server is listening on ${process.env.HTTP_PORT}`)
+    console.log(`server is listening on ${process.env.HTTP_PORT}`)
 })
