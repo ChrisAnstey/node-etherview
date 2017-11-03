15719da9ee32b34496c1825f3f8ac22f48f503f9
Chris Anstey <chris@cpanstey.co.uk>
2017-11-03 13:23:53 +0000

    Use express to get block number, and format urls nicely.

--- server.js
+++ server.js
@@ -3,7 +3,6 @@
  */
 require('dotenv').config()
 
-const url = require('url');
 const Web3 = require('web3');
 const ejs = require('ejs')
 
@@ -16,19 +15,16 @@ var app = express()
  * @param  {[type]} response [description]
  * @return {[type]}          [description]
  */
-app.get('/', (request, response) => {
+app.get('/blocks/:block', (request, response) => {
     response.writeHead(200, {
         'content-type': 'text/html'
     });
-
-    // pass true as 2nd param to parse querystring too
-    var reqDeets = url.parse(request.url, true)
 
     // create connection to geth node - in this case, the docker host
     var web3 = new Web3(new Web3.providers.HttpProvider(process.env.WEB3_PROVIDER))
 
     // load the block info using web3
-    var block = web3.eth.getBlock(reqDeets.query.block)
+    var block = web3.eth.getBlock(request.params.block)
 
     ejs.renderFile(__dirname + '/views/pages/block.ejs', {
             title: 'View Block: ' + block.number,
